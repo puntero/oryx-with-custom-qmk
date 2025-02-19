ad74573048e0406e27f0e5fffbf111a6565b48e3
puntero <angelpuntero@gmail.com>
2025-02-18 18:02:28 -0600

    exception for thumbs

--- bV7K7/features/achordion.c
+++ bV7K7/features/achordion.c
@@ -335,6 +335,15 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
                                            keyrecord_t* tap_hold_record,
                                            uint16_t other_keycode,
                                            keyrecord_t* other_record) {
+  
+  case K41:
+      break;
+  case K42:
+      break;
+  case K91:
+      break;
+  case K92:
+      break;    
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
