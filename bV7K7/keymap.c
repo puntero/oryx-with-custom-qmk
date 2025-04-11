dee9b1c43383d472113f10117c6763dd163f8c2e
Angel <angelpuntero@gmail.com>
2025-04-11 14:44:52 -0500

    added chordal_hold_layout

--- bV7K7/keymap.c
+++ bV7K7/keymap.c
@@ -207,6 +207,19 @@ tap_dance_action_t tap_dance_actions[] = {
         [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
 };
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT_voyager(
+    // Row 0 (12 keys): left half keys (first 6) are 'L', right half keys (last 6) are 'R'
+    'L', 'L', 'L', 'L', 'L', 'L',   'R', 'R', 'R', 'R', 'R', 'R',
+    // Row 1 (12 keys)
+    'L', 'L', 'L', 'L', 'L', 'L',   'R', 'R', 'R', 'R', 'R', 'R',
+    // Row 2 (12 keys)
+    'L', 'L', 'L', 'L', 'L', 'L',   'R', 'R', 'R', 'R', 'R', 'R',
+    // Row 3 (12 keys)
+    'L', 'L', 'L', 'L', 'L', 'L',   'R', 'R', 'R', 'R', 'R', 'R',
+    // Thumb row (4 keys): first 2 keys left, last 2 keys right
+    '*', '*', '*', '*'
+);
+
 bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
                        uint16_t other_keycode, keyrecord_t* other_record) {
     switch (tap_hold_keycode) {
